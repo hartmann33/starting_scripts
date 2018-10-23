cd3055e8a6fa297abd6261e174e46cc8547de48c
hartmann33 <michaelhartmann33@gmail.com>
2018-10-23 13:39:59 -0700

    Second commit

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ import requests
 response = requests.get('http://httpbin.org/ip')
 
 print('Your IP is {0}'.format(response.json()['origin']))
+
+print("That's how it all works...edit for github testing")
